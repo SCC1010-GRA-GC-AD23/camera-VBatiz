ffe705e963c01f7827afdb5cb93859aff4c78b31
Víctor Manuel Bátiz Beltrán <51245128+vbatiz@users.noreply.github.com>
2023-11-15 22:45:09 +0000

    Se revisa tema

--- sketch.js
+++ sketch.js
@@ -9,9 +9,13 @@ function setup() {
 
 function draw() {
   // put drawing code here
-  background(255)
-  rotateX(frameCount * 0.01)
-  rotateY(frameCount * 0.01)
-  rotateZ(frameCount * 0.01)
+  background(0)
+  //rotateX(frameCount * 0.01)
+  //rotateY(frameCount * 0.01)
+  //rotateZ(frameCount * 0.01)
+  camera(mouseX, mouseY, (height/2) / tan(PI/6), width/2, height/2, 0, 0, 1, 0)
+  translate(width/2, height/2, -100)
+  //stroke(255)
+  //noFill()
   box(150)
 }
